dddce68a51de971a74a7798e88b72f5b0173ee3c
anushkach0401 <anushkachakraborty0401@gmail.com>
2026-03-07 18:35:11 +0530

    colour changing shape

---
+++ vpy.py
@@ -0,0 +1,27 @@
+from vpython import *
+from time import *
+b= box(color=color.yellow)
+sleep(1)
+b.color=color.blue
+sleep(1)
+b.color=color.green
+sleep(1)
+b.color=color.red
+sleep(1)
+b.color=color.cyan
+sleep(1)
+b.color=color.magenta
+sleep(1)
+b.color=color.orange
+sleep(1)
+b.color=color.pink
+sleep(1)
+b.color=color.gray
+sleep(1)
+b.color=color.white
+sleep(1)
+b.color=color.black
+while True:
+    pass
+
+
