2a64572d3310c83d60e32f4ae8897668ac720a58
anushkach0401 <anushkachakraborty0401@gmail.com>
2026-03-21 13:04:31 +0530

    comparing in func

---
+++ babam_313/other_code/func.py
@@ -0,0 +1,10 @@
+def greet(lang):
+  if lang == 'es':
+    return
+  elif lang == 'fr':
+    return
+  else:
+    return
+print(greet('en'), 'gleen')
+print(greet("es"), 'sally')
+print(greet('fr'), 'michael')
