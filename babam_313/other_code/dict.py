0b8089823a6d739262e543e8045d8b4806943eec
anushkach0401 <anushkachakraborty0401@gmail.com>
2026-03-28 11:07:18 +0530

    dict and list eg

---
+++ babam_313/other_code/dict.py
@@ -0,0 +1,15 @@
+#list=["apple","mango","banana"]
+#list.sort()
+#print(list)
+#(a,b)=(44,'66+99')
+#print(b)
+#dict={'mark':2,'Steve':4}
+#for(k,v) in dict.items():
+ #   print(k,v)
+#dict={'a':1,'b':2,'r':3,'p':4,'c':5}
+#print(sorted(dict.items()))
+#list=[1,4,3,6,2]
+#list.reverse()
+#print(list)
+yo=set()
+print(type(yo))
