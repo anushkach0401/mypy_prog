b589be43803fc4e41b3c1af6ad97970b4efe4568
anushkach0401 <anushkachakraborty0401@gmail.com>
2026-05-10 14:52:24 +0530

    practice prblms

---
+++ babam_313/other_code/files.py
@@ -0,0 +1,5 @@
+cv=open("CV.pdf")
+for w in cv:
+    print(w)
+    
+
---
+++ babam_313/other_code/numpy_prac.py
@@ -0,0 +1,5 @@
+import numpy as np
+word="My name is Anushka.I read in IISER TVM. I like Physics"
+print(word.replace('Physics','Astrophysics'),end="\n")
+x=3.142
+print(x.degree(x))
